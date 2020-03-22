c2d962fcbf1e3083d8958e95fa27c08ee95771b4
Dylan Le <dle8@u.rochester.edu>
2020-03-22 18:37:47 -0400

    Define chrono namespace

--- chrono.cpp
+++ chrono.cpp
@@ -1,4 +1,11 @@
 #include <chrono>
 
 using namespace std::chrono;
-typedef high_resolution_clock::time_point hrc_time;
+
+namespace chrono {
+    typedef high_resolution_clock::time_point hrc_time;
+
+    double elasped_time(hrc_time t1, hrc_time t2) {
+        return (t2 - t1).count();
+    }
+}
